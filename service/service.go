700165db230f885a7cb5575d15b4f93538309886
Matthew Vishnyakov <125650555+Tr0ubad0ur@users.noreply.github.com>
2025-07-09 10:35:31 +0300

    Update service.go

--- service/service.go
+++ service/service.go
@@ -1,1 +1,35 @@
+package tgservice
 
+import (
+	"fmt"
+	"log"
+	"tgbot/adapter"
+)
+
+type TgAPIAdapter interface {
+	GetMessageChannel() <-chan tgadapter.IncomingMessage
+	GetCommandChannel() <-chan tgadapter.IncomingCommand
+	SendTaskList(tgadapter.SendTaskListCommand) error
+}
+
+type Service struct{
+	adapter TgAPIAdapter
+}
+
+func NewService(a TgAPIAdapter) *Service{
+	srv := &Service{adapter: a}
+
+	
+	return srv
+}
+
+func (s *Service) Serve() error{
+
+	log.Println("ServeStarted")
+	for{
+		select{
+		case msg:=<-s.adapter.GetMessageChannel():
+			fmt.Println(msg.Text)
+		}
+	}
+}
